8942403882190c6f2b84cdd89d66a3b16750878f
cdeborja <cdeborja@gmail.com>
2016-10-21 11:50:19 -0400

    Implement ES6 linter

--- components/app.jsx
+++ components/app.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import store from '../store'
-import Todos from './todos'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
+import Todos from './todos';
 
 let reactElement = document.getElementById('react');
 
@@ -11,4 +11,4 @@ render(
     <Todos />
   </Provider>,
   reactElement
-)
+);
