f0e108e2a75cf9725e89cac405d945429ee1b6ff
cdeborja <cdeborja@gmail.com>
2016-10-21 12:57:46 -0400

    Enable adding to list

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,9 +1,9 @@
 import Immutable from 'immutable';
 
-export default (state = Immutable.List(['Code More!']), action) => {
+export default (state = Immutable.List(['Add new to dos to this list!']), action) => {
   switch(action.type) {
     case 'addTodo':
-      return state.push(action.todo);
+      return state.unshift(action.todo);
     default:
       return state;
   }
