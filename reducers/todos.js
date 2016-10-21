0729059de42fc75ad7066e9524f13de771c1d2f9
cdeborja <cdeborja@gmail.com>
2016-10-21 13:02:57 -0400

    Enable remove of todo

--- reducers/todos.js
+++ reducers/todos.js
@@ -4,6 +4,8 @@ export default (state = Immutable.List(['Add new to dos to this list!']), action
   switch(action.type) {
     case 'addTodo':
       return state.unshift(action.todo);
+    case 'deleteTodo':
+      return state.filter((todo, index) => index !== action.index);
     default:
       return state;
   }
